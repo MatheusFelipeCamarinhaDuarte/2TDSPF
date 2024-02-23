03b048036244047ec6a92ff6bfa4e7eb8e81148a
Matheus <matheus.duarte.lins@gmail.com>
2024-02-23 10:36:58 -0300

    Exemplo Herança

--- FeitoEmSala_Aula3/Program.cs
+++ FeitoEmSala_Aula3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FeitoEmSala_Aula3_2TDSPF;
+
+var gato = new Gato();
+gato.EmitirSom();
+gato.Miar();
---
+++ FeitoEmSala_Aula3/encapsulamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeitoEmSala_Aula3_2TDSPF
+{
+    internal class Animal
+    {
+
+        public void EmitirSom()
+        {
+            Console.WriteLine("Um som qualquer de animal!");
+        }       
+    }
+
+    internal class Gato : Animal
+    {
+        public void Miar()
+        {
+            
+            Console.WriteLine("MIAUUU");
+        }
+    }
+}
