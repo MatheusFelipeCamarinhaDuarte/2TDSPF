336ee1cc49813502fd792fd23506a6658746fe6e
Matheus <matheus.duarte.lins@gmail.com>
2024-02-23 11:14:40 -0300

    Camadas de decisão

--- FeitoEmSala_Aula3_instrucoesBasicas/Program.cs
+++ FeitoEmSala_Aula3_instrucoesBasicas/Program.cs
@@ -8,7 +8,39 @@ bool velho;
 var testeVar = "";
 
 
+// Controladores de fluxo
 
+    // Tomada de decisão
+    if (idade == 15)
+    {
+        Console.WriteLine("Aluno tem 15 anos");
+    }
+    else
+    {
+    Console.WriteLine("Aluno não tem 15 anos");
+    }
+    
+
+    if (idade == 15)
+        Console.WriteLine("Aluno tem 15 anos");
+    else
+        Console.WriteLine("Aluno não tem 15 anos");
+
+    var mesAno = 4;
+switch (mesAno)
+{
+    case 1:
+        Console.WriteLine("Tem o feriado de ano novo");
+        break;
+    case 4:
+        Console.WriteLine("Tem feriado tiradentes");
+        break;
+    case 2:
+        Console.WriteLine("Tem feirado de Carnaval");
+        break;
+    default:
+        break;
+}
 
 
 
